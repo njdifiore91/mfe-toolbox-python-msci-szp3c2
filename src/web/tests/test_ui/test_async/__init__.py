31291b85ba47274dc743d0d323c34a5aaa05586f
njdifiore91 <nick@blitzy.com>
2025-02-27 15:08:17 -0500

    Add public-samples/mfe-toolbox-python-msci-szp3c2/src/web/tests/test_ui/test_async/__init__.py

---
+++ src/web/tests/test_ui/test_async/__init__.py
@@ -0,0 +1,6 @@
+"""
+Test package for PyQt6 asynchronous functionality.
+
+This package contains test cases for asynchronous operations in the MFE Toolbox's
+GUI components, which are implemented using PyQt6 and Python's async/await patterns.
+"""
